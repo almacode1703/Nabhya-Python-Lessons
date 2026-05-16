6b7fb038786a12add27315464455bd7499a91c7b
almacode1703 <testmygitt.2023@gmail.com>
2026-05-16 21:13:57 +0530

    updates

---
+++ lesson2-datatype/act1.py
@@ -0,0 +1,16 @@
+name = "Codingal"
+age = 13
+isStudent = True
+weight = 39
+
+print("Name : ", name)
+print("Datatype of name is ", type(name).__name__)
+
+print("Age : ", age)
+print("Datatype of age is ", type(age).__name__)
+
+print("Is Student ? : ", isStudent)
+print("Datatype of name is ", type(isStudent).__name__)
+
+print("Weight : ",weight)
+print("Datatype of name is ", type(weight).__name__)
