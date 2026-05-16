e25c198b484a640779164495598ac7ec0a60faf8
almacode1703 <testmygitt.2023@gmail.com>
2026-05-16 21:25:12 +0530

    updates

--- lesson2-datatype/act1.py
+++ lesson2-datatype/act1.py
@@ -3,6 +3,7 @@ age = 13
 isStudent = True
 weight = 39
 
+# Printing the datattypes
 print("Name : ", name)
 print("Datatype of name is ", type(name).__name__)
 
@@ -10,7 +11,16 @@ print("Age : ", age)
 print("Datatype of age is ", type(age).__name__)
 
 print("Is Student ? : ", isStudent)
-print("Datatype of name is ", type(isStudent).__name__)
+print("Datatype of isStudent is ", type(isStudent).__name__)
 
 print("Weight : ",weight)
-print("Datatype of name is ", type(weight).__name__)
+print("Datatype of weight is ", type(weight).__name__)
+
+# Typecasting
+age = str(age)
+print("Datatype of age is : ", type(age).__name__)
+
+weight = float(weight)
+print("Datatype of weight is : ", type(weight).__name__)
+print("Value of weight : ", weight)
+
