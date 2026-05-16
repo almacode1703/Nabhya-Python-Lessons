412b4a9476a8726fcaaa7f9132dc2c532cec3be3
almacode1703 <testmygitt.2023@gmail.com>
2026-05-16 21:53:53 +0530

    updates

---
+++ lesson2-datatype/act2.py
@@ -0,0 +1,18 @@
+# Operators in Python
+num1 = 25
+num2 = 32
+
+# Arithmetic Operators
+print("ADDITION : ", num1+num2)
+print("SUBTRACTION : ", num1-num2)
+print("MULTIPLICATION : ", num1*num2)
+print("DIVISION : ", num1/num2)
+print("REMAINDER : ", num1%num2)
+
+# Comparison Operator
+print("Equality : ", num1 == num2)
+print("Greater than : ", num1 > num2)
+print("Less Than : ", num1 < num2)
+print("Greater than or Equals to : ", num1 >= num2)
+print("Less than or Equals to : ", num1 <= num2)
+print(" Not Equl to ", num1 != num2)
---
+++ lesson2-datatype/act3.py
@@ -0,0 +1,19 @@
+# String Operations
+first_name = "John"
+last_name = "Shukla"
+fullname = first_name + " " + last_name
+
+test = " Haa"
+print(fullname)
+print(test*5)
+
+word = 'computer'
+print(word)
+print("First Letter : ", word[0])
+print("Last Letter : ", word[-1])
+print("Middle Letter : ", word[3:5])
+print("Entire word : ", word[:])
+print("Reverse : ", word[::-1])
+print("Reverse : ", word[:4])
+print("Reverse : ", word[2:])
+
